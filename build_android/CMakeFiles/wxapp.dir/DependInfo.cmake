40abe3acf3b1adb49582455162c653dc91f683e1
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-26 13:36:31 -0300

    :add: Adiciona o arquivo build_android/CMakeFiles/wxapp.dir/DependInfo.cmake

---
+++ build_android/CMakeFiles/wxapp.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/obs/kiko/android/cpp/wx_app.cpp" "CMakeFiles/wxapp.dir/wx_app.cpp.o" "gcc" "CMakeFiles/wxapp.dir/wx_app.cpp.o.d"
+  "/workspace/obs/kiko/android/cpp/wx_jni.cpp" "CMakeFiles/wxapp.dir/wx_jni.cpp.o" "gcc" "CMakeFiles/wxapp.dir/wx_jni.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
