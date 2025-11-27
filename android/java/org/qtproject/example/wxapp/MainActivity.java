d0f96f9761ebac61cca870f1f35ac5b91863d020
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-26 23:43:50 -0300

    :style: Atualiza o estilo do arquivo android/java/org/qtproject/example/wxapp/MainActivity.java

--- android/java/org/qtproject/example/wxapp/MainActivity.java
+++ android/java/org/qtproject/example/wxapp/MainActivity.java
@@ -1,10 +1,26 @@
-@Override
-public void onWindowFocusChanged(boolean hasFocus) {
-    super.onWindowFocusChanged(hasFocus);
+package org.qtproject.example.wxapp;
 
-    if (hasFocus && !wxStarted) {
-        wxStarted = true;
-        WxBridge.logInitCalled();   // <-- log Java
-        WxBridge.initWx();          // chama JNI
+import org.qtproject.qt5.android.bindings.QtActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends QtActivity {
+
+    static {
+        System.loadLibrary("wxapp");  // carrega libwxapp.so
+    }
+
+    private static boolean wxStarted = false;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus && !wxStarted) {
+            wxStarted = true;
+            Log.d("WXAPP", "MainActivity.onWindowFocusChanged: chamando initWx()");
+            WxBridge.initWx();
+        }
     }
 }
+
