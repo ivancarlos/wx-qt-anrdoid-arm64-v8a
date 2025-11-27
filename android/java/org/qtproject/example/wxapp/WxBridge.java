045c5ddd64a7c6c14eeab9a6e5222e116db2b3ae
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-26 22:00:04 -0300

    :style: Atualiza o estilo do arquivo android/java/org/qtproject/example/wxapp/WxBridge.java

--- android/java/org/qtproject/example/wxapp/WxBridge.java
+++ android/java/org/qtproject/example/wxapp/WxBridge.java
@@ -1,6 +1,11 @@
 package org.qtproject.example.wxapp;
 
+import android.util.Log;   // <-- adiciona isso
+
 public class WxBridge {
     public static native void initWx();
-}
 
+    public static void logInitCalled() {
+        Log.d("WXAPP", "WxBridge.logInitCalled() chamado");
+    }
+}
